1587dab4483e28c4327dd7dc21dd81b4cc530741
Olexii <bezvik5@gmail.com>
2021-04-01 21:13:29 +0300

    finished

--- script/script.js
+++ script/script.js
@@ -12,6 +12,36 @@ if(menuIcon){
 
 };
 
+//Прокрутка к нужному пункту меню при клике
+ 
+const menuLinks = document.querySelectorAll('.menu-link[data-goto]');
+
+if(menuLinks.length>0){
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuLinkClick)
+    });
+
+    function onMenuLinkClick(e){
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+
+            if(menuIcon.classList.contains('active')){
+                document.body.classList.remove('lock'); //при открытом меню не скролится страничка
+                menuIcon.classList.remove('active');
+                menuHeader.classList.remove('active');
+            }
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            })
+            e.preventDefault();
+        }
+    }
+}
+
 
 
 
